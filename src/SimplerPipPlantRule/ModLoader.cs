02fe8c6b51d95ca71c03bbd0ea75e696f7a11596
limc <ancienlee@qq.com>
2021-07-06 23:33:30 +0800

    SimplerPipPlantRule

--- src/SimplerPipPlantRule/ModLoader.cs
+++ src/SimplerPipPlantRule/ModLoader.cs
@@ -1,21 +1,15 @@
-using AsLimc.commons;
+using AsLimc.Commons;
 using HarmonyLib;
 using KMod;
 using PeterHan.PLib.Core;
-using PeterHan.PLib.Options;
 
-namespace AsLimc.SimplerPipPlantRule
-{
-    public class ModLoader : UserMod2
-    {
-        public override void OnLoad(Harmony harmony)
-        {
+namespace AsLimc.SimplerPipPlantRule {
+    public class ModLoader : UserMod2 {
+        public override void OnLoad(Harmony harmony) {
             base.OnLoad(harmony);
             PUtil.InitLibrary();
-            new POptions().RegisterOptions(this,typeof(Settings));
-            // POptions.RegisterOptions(this,typeof(Settings));
             VLib.Init();
-            Settings.Init();
+            Settings.Init(this);
         }
     }
 }
--- src/SimplerPipPlantRule/Settings.cs
+++ src/SimplerPipPlantRule/Settings.cs
@@ -1,3 +1,4 @@
+using KMod;
 using Newtonsoft.Json;
 using PeterHan.PLib.Options;
 
@@ -8,11 +9,12 @@ namespace AsLimc.SimplerPipPlantRule
     [JsonObject(MemberSerialization.OptIn)]
     internal class Settings
     {
-        private static Settings _Instance;
-        public static Settings Get() => _Instance;
+        private static Settings _INSTANCE;
+        public static Settings Get() => _INSTANCE;
 
-        public static void Init() {
-            _Instance ??= POptions.ReadSettings<Settings>() ?? new Settings();
+        public static void Init(UserMod2 modLoader) {
+            new POptions().RegisterOptions(modLoader, typeof(Settings));
+            _INSTANCE ??= POptions.ReadSettings<Settings>() ?? new Settings();
         }
         
         [Option("Search Min Interval (seconds)", "min interval for seed search.", null)]
